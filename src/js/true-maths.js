b57fce32b5c2a4ae16e0d743d12c693507df55c2
Alexey <leshaego14@gmail.com>
2020-06-30 16:02:21 +0300

    Auto stash before merge of "master" and "origin/master"

--- src/js/true-maths.js
+++ src/js/true-maths.js
@@ -5,56 +5,109 @@ let currentTeta;
 let currentN;
 
 $(document).ready(function () {
+    paper.setup(document.getElementById("screen"));
+
     currentA = Number($('#a-input').val());
     currentB = Number($('#b-input').val());
     currentLambda = Number($('#lambda-input').val());
-    currentTeta = Number($('#teta-input').val());
+    currentTeta = radians(Number($('#teta-input').val()));
     currentN = Number($('#n-input').val());
 
-    $('#a-input').change(function () {
+    $('#a-input').on('input', function () {
         currentA = Number($(this).val());
+        drawScreen();
     });
 
-    $('#b-input').change(function () {
+    $('#b-input').on('input', function () {
         currentB = Number($(this).val());
+        drawScreen();
     });
 
-    $('#n-input').change(function () {
+    $('#n-input').on('input', function () {
         currentN = Number($(this).val());
+        drawScreen();
     });
 
-    $('#teta-input').change(function () {
-        currentTeta = Number($(this).val());
+    $('#teta-input').on('input', function () {
+        currentTeta = radians(Number($(this).val()));
+        drawScreen();
     });
 
-    $('#lambda-input').change(function () {
+    $('#lambda-input').on('input', function () {
         currentLambda = Number($(this).val());
+        drawScreen();
     });
 
-    paper.setup(document.getElementById("screen"));
+    drawScreen();
+});
+
+function radians(teta) {
+    return 0.00174533 * teta * 10;
+}
+
+function drawScreen() {
+    paper.project.clear();
 
     let oneLineWidth = oneLineWidthFunction(currentA, currentB, currentLambda, currentTeta, currentN);
     let hueLambda = hueLambdaFunction(currentLambda);
 
-    console.log(oneLineWidth + " + " + hueLambda);
+    let colors = getColors(oneLineWidth, hueLambda);
 
+    let point = new paper.Point({
+        x: 200,
+        y: 0
+    });
+
+    createLeftRectangle(oneLineWidth, point, colors);
+    createRightRectangle(oneLineWidth, point, colors);
+    // let rectangleRight = new paper.Path.Rectangle({
+    //     topLeft: point,
+    //     size: {
+    //         width: oneLineWidth / 2,
+    //         height: 500
+    //     },
+    //     fillColor: {
+    //         origin: point,
+    //         destination: point.add({ x: oneLineWidth / 2, y: 0}),
+    //         gradient: {
+    //             stops: colors,
+    //             radial: false
+    //         }
+    //     }
+    // });
+    // let rectangleLeft = new paper.Path.Rectangle({
+    //     topRight: point,
+    //     size: {
+    //         width: oneLineWidth / 2,
+    //         height: 500
+    //     },
+    //     fillColor: {
+    //         origin: point,
+    //         destination: point.add({ x: -oneLineWidth / 2, y: 0}),
+    //         gradient: {
+    //             stops: colors,
+    //             radial: false
+    //         }
+    //     }
+    // });
+    paper.view.draw();
+}
+
+function getColors(oneLineWidth, hueLambda) {
     let colors = [];
     for (let x = 0; x < oneLineWidth; x+= oneLineWidth / 100) {
         colors.push({
             hue: hueLambda,
             saturation: 1,
             brightness: brightnessWidth(oneLineWidth, x)
-            // brightness: 1
         });
     }
 
-    console.log(colors);
+    return colors;
+}
 
-    let point = new paper.Point({
-        x: 100,
-        y: 0
-    });
-    let rectangleRight = new paper.Path.Rectangle({
+function createLeftRectangle(oneLineWidth, point, colors) {
+    new paper.Path.Rectangle({
         topLeft: point,
         size: {
             width: oneLineWidth / 2,
@@ -62,33 +115,41 @@ $(document).ready(function () {
         },
         fillColor: {
             origin: point,
-            destination: point.add({ x: oneLineWidth / 2, y: 0}),
+            destination: point.add({
+                x: oneLineWidth / 2,
+                y: 0
+            }),
             gradient: {
                 stops: colors,
                 radial: false
             }
         }
     });
-    let rectangleLeft = new paper.Path.Rectangle({
-        topRight: point,
+}
+
+function createRightRectangle(oneLineWidth, point, colors) {
+    new paper.Path.Rectangle({
+        topLeft: point,
         size: {
             width: oneLineWidth / 2,
             height: 500
         },
         fillColor: {
             origin: point,
-            destination: point.add({ x: -oneLineWidth / 2, y: 0}),
+            destination: point.add({
+                x: -oneLineWidth / 2,
+                y: 0
+            }),
             gradient: {
                 stops: colors,
                 radial: false
             }
         }
     });
-    paper.view.draw();
-});
+}
 
 function oneLineWidthFunction(a, b, lambda, teta, n) {
-    return (a + b) * lambda / (2 * a * teta * (n - 1));
+    return (a + b) * lambda * (1e-3) / (2 * a * teta * (n - 1));
 }
 
 function pictureWidth(b, teta, n) {
@@ -96,13 +157,50 @@ function pictureWidth(b, teta, n) {
 }
 
 function hueLambdaFunction(lambda) {
-    return 0.7 * (780 - lambda);
+    //красный 0 - 30
+    if (lambda >= 630) {
+        return 0.2 * (780 - lambda);
+    }
+
+    //оранжевый 30 - 50
+    if (lambda < 630 && lambda >= 590) {
+        return 30 + 0.5 * (630 - lambda);
+    }
+
+    //желтый 50 - 70
+    if (lambda < 590 && lambda >= 570) {
+        return 50 + (590 - lambda);
+    }
+
+    //желто-зеленый 70 - 90
+    if (lambda < 570 && lambda >= 550) {
+        return 70 + (570 - lambda);
+    }
+
+    //зеленый 90 - 150
+    if (lambda < 550 && lambda >= 510) {
+        return 90 + 1.5 * (550 - lambda);
+    }
+
+    //голубой 150 - 200
+    if (lambda < 510 && lambda >= 480) {
+        return 150 + 5/3 * (550 - lambda);
+    }
+
+    //синий 200 - 250
+    if (lambda < 480 && lambda >= 450) {
+        return 200 + 5/3 * (480 - lambda);
+    }
+
+    //фиолетовый 250 - 280
+    //lambda 450 - 380
+    return 250 + 3/7 * (450 - lambda);
 }
 
 // width of interferencionniy line
-// x - from 0 to 0.5 width
+// x - from 0 to 1 width
 function brightnessWidth(width, x) {
     let x_percent = x / width * 100;
 
-    return x_percent > 60 ? 0.01 * (100 - (x_percent - 60)) : 1;
+    return x_percent > 40 ? 0.01 * (100 - (x_percent - 40)) : 1;
 }
