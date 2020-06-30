74ee138510eac76ea61fb610273b91b989624e83
Alexey <leshaego14@gmail.com>
2020-06-30 13:14:41 +0300

    psch

--- src/true-maths.js
+++ src/true-maths.js
@@ -1,3 +1,11 @@
-function oneLineWidth() {
-
+function oneLineWidth(a, b, lambda, teta, n) {
+    return (a + b) * lambda / (2 * a * teta * (n - 1));
 }
+
+function pictureWidth(b, teta, n) {
+    return 2 * b * teta * (n - 1);
+}
+
+$(document).ready(function () {
+    paper
+});
