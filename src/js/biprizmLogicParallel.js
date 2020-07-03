d7869d9d53038a3fde114bc2c1ddeee7a144deab
Alexey <leshaego14@gmail.com>
2020-07-03 13:03:06 +0300

    psch

--- src/js/biprizmLogicParallel.js
+++ src/js/biprizmLogicParallel.js
@@ -38,20 +38,42 @@ function drawLines(screenPosition) {
     secondPath.moveTo(new paper.Point(0, 250 + currentA * 100 * 170 / 2));
     secondPath.lineTo(new paper.Point(250, 250 + currentA * 100 * 170 / 2));
 
-    console.log(currentRombLength);
+    let additionalFirstPath = new paper.Path();
+    // additionalFirstPath.dashArray = [10, 12];
+    additionalFirstPath.strokeColor = 'white';
+    additionalFirstPath.moveTo(new paper.Point(0, 250 - currentA * 100 * 170 / 4));
+    additionalFirstPath.lineTo(new paper.Point(275, 250 - currentA * 100 * 170 / 4));
+
+    let additionalSecondPath = new paper.Path();
+    // additionalSecondPath.dashArray = [10, 12];
+    additionalSecondPath.strokeColor = 'white';
+    additionalSecondPath.moveTo(new paper.Point(0, 250 + currentA * 100 * 170 / 4));
+    additionalSecondPath.lineTo(new paper.Point(275, 250 + currentA * 100 * 170 / 4));
 
     //third line
     let thirdPath = new paper.Path();
     thirdPath.strokeColor = 'white';
-    console.log("currentA: " + currentA);
     thirdPath.moveTo(new paper.Point(250, 250 - currentA * 100 * 170 / 2));
     thirdPath.lineTo(new paper.Point(currentRombLength * 200 + 300, 250));
+
+    let additionalThirdPath = new paper.Path();
+    // additionalThirdPath.dashArray = [10, 12];
+    additionalThirdPath.strokeColor = 'white';
+    additionalThirdPath.moveTo(new paper.Point(275, 250 - currentA * 100 * 170 / 4));
+    additionalThirdPath.lineTo(new paper.Point(600 + 300, 170 * currentA * (1 / (4 * currentRombLength + 1) * 600 - 275 / (4 * currentRombLength + 1) + 25) + 240));
+    // additionalThirdPath.lineTo(new paper.Point(currentRombLength * 200 / 2 + 300, 250));
 
     //fourth line
     let fourthPath = new paper.Path();
     fourthPath.strokeColor = 'white';
     fourthPath.moveTo(new paper.Point(250, 250 + currentA * 100 * 170 / 2));
     fourthPath.lineTo(new paper.Point(currentRombLength * 200 + 300, 250));
+
+    let additionalFourthPath = new paper.Path();
+    // additionalFourthPath.dashArray = [10, 12];
+    additionalFourthPath.strokeColor = 'white';
+    additionalFourthPath.moveTo(new paper.Point(275, 250 + currentA * 100 * 170 / 4));
+    additionalFourthPath.lineTo(new paper.Point(600 + 300, 250 - ((170 * currentA * (1 / (4 * currentRombLength + 1) * 600 - 275 / (4 * currentRombLength + 1) + 25) + 240) - 250)));
 
     //fifth line
     let fifthPath = new paper.Path();
